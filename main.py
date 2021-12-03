c5e428f650952f8534c3458ed88b9a93ea0414ad
Bhargav <vicharapubhargav462981609@gmail.com>
2021-12-03 12:54:41 +0530

    Main File Edited

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running"
+    return "Hi Guys I am Bhargav...."
 
 port = int(os.getenv("PORT", 5001))
 
